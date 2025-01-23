6e9c48e4ae030fa12a48f135392ff0adea54e7a5
chhetri0 <nikeshbaniya@mail.weber.edu>
2025-01-23 10:09:36 -0700

    ADD: input for player name

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
+
 # Welcome message and introductions
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
 
+#ASK for the player naame
+player_name = input("What is your name, adventurer?")
 
+#concatenate strings to create a personalized mess
+print("Welcome, "+ player_name + "!Your journey begins now.")
 
